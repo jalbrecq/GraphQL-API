03ebdd13d0253a526c2d0ccab8a1d77371da4b9a
Jean <jeanalbrecq@gmail.com>
2020-08-07 08:21:51 +0200

    Updates

---
+++ precache-manifest.90fa7660d024d553bb34952062539d3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334419a38f5fc8832aa7cd7acf409f5b",
+    "url": "/GraphQL-API/index.html"
+  },
+  {
+    "revision": "52e97496eb091cd1bf72",
+    "url": "/GraphQL-API/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b63083ed0eb1c68cd10f",
+    "url": "/GraphQL-API/static/css/main.c2998ded.chunk.css"
+  },
+  {
+    "revision": "52e97496eb091cd1bf72",
+    "url": "/GraphQL-API/static/js/2.e00e036f.chunk.js"
+  },
+  {
+    "revision": "572bfeb62474f0cec44e259ca5a99f18",
+    "url": "/GraphQL-API/static/js/2.e00e036f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63083ed0eb1c68cd10f",
+    "url": "/GraphQL-API/static/js/main.02870eba.chunk.js"
+  },
+  {
+    "revision": "7d1dfe7dbdf432b42895",
+    "url": "/GraphQL-API/static/js/runtime-main.a2484b8d.js"
+  }
+]);
