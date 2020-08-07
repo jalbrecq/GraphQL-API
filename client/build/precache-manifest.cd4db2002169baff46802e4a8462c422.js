92496fb39fecd52a565f3cfe515b0f0ab18c98ea
Jean <jeanalbrecq@gmail.com>
2020-08-07 11:35:47 +0200

    push build

---
+++ client/build/precache-manifest.cd4db2002169baff46802e4a8462c422.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2735abab833aa3a94f18c1b9b1d79d39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61a6e02c2d68b1bdf16a",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b63083ed0eb1c68cd10f",
+    "url": "/static/css/main.c2998ded.chunk.css"
+  },
+  {
+    "revision": "61a6e02c2d68b1bdf16a",
+    "url": "/static/js/2.e00e036f.chunk.js"
+  },
+  {
+    "revision": "572bfeb62474f0cec44e259ca5a99f18",
+    "url": "/static/js/2.e00e036f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63083ed0eb1c68cd10f",
+    "url": "/static/js/main.02870eba.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
